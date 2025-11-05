bfd528169cf99bc98de2cd359cfd375b4cdaac01
VictorSaraviaUFM <victorsaravia@ufm.edu>
2025-11-04 21:35:21 -0600

    Creación de React

---
+++ FUTSTATS/futstats/src/pages/PlayerPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayerPage = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Lionel Messi</h2>
+      <p>Estadísticas, gráficos y comentarios irán aquí.</p>
+    </div>
+  );
+};
+
+export default PlayerPage;
---
+++ FUTSTATS/futstats/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Mi Perfil Futbolero</h2>
+      <p>Aquí podrás ver tus favoritos y actividad.</p>
+    </div>
+  );
+};
+
+export default Profile;
